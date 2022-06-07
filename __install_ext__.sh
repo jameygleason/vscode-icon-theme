de4b8625005b69563af72232b5bca5d17423df0e
jameygleason <james.p.gleason@gmail.com>
2022-06-07 12:32:11 -0400

    initial

---
+++ __install_ext__.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -r . ~/.vscode/extensions/pkief.material-icon-theme-4.10.0
---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+git remote set-url origin git@github.com:jameygleason/vscode-icon-theme.git
+git push origin
+git remote set-url origin git@gitlab.com:jameygleason/vscode-icon-theme.git
+git push origin
